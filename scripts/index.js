516a3a2c260828e1612c42f4b01e6d304e0a0367
Aris <64918822+Arisamiga@users.noreply.github.com>
2023-05-14 12:15:39 +0100

    Added wttr and goweather

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,196 @@
-function search(){
-    const city = document.getElementById('city').value;
-    console.log(city)
-    // Call the openweatherapp API
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9de243494c0b295cca9337e1e96b00e2&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-        writeResults(data)
-    }
-    )
-    .catch(err => {
-    alert("Wrong city name")
-    console.log(err)
-})
-    
+function search() {
+  const city = document.getElementById("city").value;
+  console.log(city);
+  // Call wttr API
+  fetch(`https://wttr.in/${city}?format=j1`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      writeResults(
+        data,
+        "wttr",
+        data.current_condition[0].FeelsLikeC + " °C",
+        data.current_condition[0].windspeedKmph + " km/h",
+        data.current_condition[0].weatherDesc[0].value,
+        data.current_condition[0].humidity,
+        data.current_condition[0].pressure,
+        toWeatherIcon(data.current_condition[0].weatherDesc[0].value)
+      );
+    })
+    .catch((err) => {
+      alert("4Wrong city name");
+      console.log(err);
+    });
+  // Call goweatherapp API
+  fetch(`https://goweather.herokuapp.com/weather/${city}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      writeResults(
+        data,
+        "goW",
+        data.temperature,
+        data.wind,
+        data.description,
+        "Not available",
+        "Not available",
+        toWeatherIcon(data.description)
+      );
+    })
+    .catch((err) => {
+      alert("3Wrong city name");
+      console.log(err);
+    });
+  // Call the openweatherapp API
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9de243494c0b295cca9337e1e96b00e2&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      writeResults(
+        data,
+        "openWM",
+        data.main.temp + " °C",
+        data.wind.speed,
+        data.weather[0].main,
+        data.main.humidity,
+        data.main.pressure,
+        data.weather[0]["icon"]
+      );
+    })
+    .catch((err) => {
+      alert("2Wrong city name");
+      console.log(err);
+    });
 }
 
-function writeResults(data){
-    console.log(data)
-    if(data.cod != 200) return alert("Wrong city name");
-    let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
-    const { main, name, sys, weather } = data;
-    console.log(data)
-    document.getElementById('result').style.display = "block"
-    document.getElementsByClassName('openWM')[0].style.display = "block"
-    document.getElementById('temp').innerHTML = "Temperature: " + data.main.temp
-    document.getElementById('city-name').innerHTML = data.name
-    document.getElementById('country').innerHTML = regionNames.of(data.sys.country)
-    document.getElementById('weather').innerHTML = data.weather[0].main
-    document.getElementById('humidity').innerHTML = "Humidity: " + data.main.humidity
-    document.getElementById('pressure').innerHTML = "Pressure: " + data.main.pressure
-    document.getElementById('wind').innerHTML = "Wind: " + data.wind.speed
-    document.getElementById('weather-icon').src = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`
+function toWeatherIcon(icon) {
+  switch (icon) {
+    case "Sunny":
+      return "01d";
+    case "Clear":
+      return "01d";
+    case "Partly cloudy":
+      return "02d";
+    case "Cloudy":
+      return "03d";
+    case "Overcast":
+      return "04d";
+    case "Mist":
+      return "50d";
+    case "Patchy rain possible":
+      return "09d";
+    case "Patchy snow possible":
+      return "13d";
+    case "Patchy sleet possible":
+      return "13d";
+    case "Patchy freezing drizzle possible":
+      return "13d";
+    case "Thundery outbreaks possible":
+      return "11d";
+    case "Blowing snow":
+      return "13d";
+    case "Blizzard":
+      return "13d";
+    case "Fog":
+      return "50d";
+    case "Freezing fog":
+      return "50d";
+    case "Patchy light drizzle" ||
+      "Patchy light rain" ||
+      "Light drizzle" ||
+      "Freezing drizzle" ||
+      "Heavy freezing drizzle" ||
+      "Light rain" ||
+      "Moderate rain at times" ||
+      "Moderate rain" ||
+      "Heavy rain at times" ||
+      "Heavy rain" ||
+      "Light freezing rain" ||
+      "Moderate or heavy freezing rain":
+      return "09d";
+    case "Light sleet" ||
+      "Moderate or heavy sleet" ||
+      "Patchy light snow" ||
+      "Light snow" ||
+      "Patchy moderate snow" ||
+      "Moderate snow" ||
+      "Patchy heavy snow" ||
+      "Heavy snow" ||
+      "Ice pellets":
+      return "13d";
+    case "Light rain shower":
+      return "09d";
+    case "Moderate or heavy rain shower":
+      return "09d";
+    case "Torrential rain shower":
+      return "09d";
+    case "Light sleet showers":
+      return "13d";
+    case "Moderate or heavy sleet showers":
+      return "13d";
+    case "Light snow showers":
+      return "13d";
+    case "Moderate or heavy snow showers":
+      return "13d";
+    case "Light showers of ice pellets":
+      return "13d";
+    case "Moderate or heavy showers of ice pellets":
+      return "13d";
+    case "Patchy light rain with thunder":
+      return "11d";
+    case "Moderate or heavy rain with thunder":
+      return "11d";
+    case "Patchy light snow with thunder":
+      return "11d";
+    case "Moderate or heavy snow with thunder":
+      return "11d";
+    default:
+      return "01d";
+  }
 }
 
-document.addEventListener('click', function(e){
-    if(e.target && e.target.className == 'openWM'){
-        if(document.getElementById('openWM-R').style.display == "block"){
-            document.getElementById('openWM-R').style.display = "none"
-            return;
-        }
-        document.getElementById('openWM-R').style.display = "block"
+function writeResults(
+  data,
+  wp,
+  temp,
+  wind,
+  description,
+  humidity,
+  pressure,
+  icon
+) {
+  console.log(wp, data);
+  //   if (data.cod != 200 && !data.wind) return alert("1Wrong city name");
+  let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+  document.getElementById("result").style.display = "block";
+  document.getElementsByClassName(wp)[0].style.display = "block";
+  document.getElementById(wp + "-temp").innerHTML = "Temperature: " + temp;
+  if (!document.getElementById("country").innerHTML) {
+    document.getElementById("city-name").innerHTML = data.name;
+    document.getElementById("country").innerHTML = regionNames.of(
+      data.sys.country
+    );
+  }
+  document.getElementById(wp + "-weather").innerHTML = description;
+  document.getElementById(wp + "-humidity").innerHTML = "Humidity: " + humidity;
+  document.getElementById(wp + "-pressure").innerHTML = "Pressure: " + pressure;
+  document.getElementById(wp + "-wind").innerHTML = "Wind: " + wind;
+  if (!icon) return console.log("no icon");
+  document.getElementById(
+    wp + "-weather-icon"
+  ).src = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${icon}.svg`;
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.className.includes("rWP")) {
+    console.log(e.target.className.replace("rWP", "") + "-R");
+    const dropdown =
+      e.target.className.replace("rWP", "").replace(" ", "") + "-R";
+    if (document.getElementById(dropdown).style.display == "block") {
+      document.getElementById(dropdown).style.display = "none";
+      return;
     }
-})
+    document.getElementById(dropdown).style.display = "block";
+  }
+});
